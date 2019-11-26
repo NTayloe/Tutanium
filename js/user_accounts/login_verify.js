6ac25503b8b118257e1aff281e46b80828c25786
Connor Gilliom <gillce01@pfw.edu>
2019-11-26 01:46:07 +0000

    Revert "User account db connection"

--- js/user_accounts/login_verify.js
+++ js/user_accounts/login_verify.js
@@ -1,12 +1,3 @@
 function verify(form){
-    
-    if(document.getElementById("username").value === ""){
-        alert("You must enter a username");
-        return false;
-    }else if(document.getElementById("password").value === ""){
-        alert("You must enter a password");
-        return false;
-    }
-
-    return true;
+    return false;
 }
